62bb80846e3c74973dc65d502c481f8bcc5e0cbf
AlexM <alex42@thesandbox.net>
2023-11-06 17:35:46 -0800

    Enable offline access

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def create_messages_client():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8080, open_browser=False)
+            creds = flow.run_local_server(port=8080, open_browser=False, access_type='offline')
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
